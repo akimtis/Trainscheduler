e66906df844390722104a5beea3f6835c53d3f6c
Anna Kimtis <Anna@Annas-MacBook-Pro.local>
2017-01-05 18:50:57 -0500

    added to js

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,4 @@
 
-
   var config = {
     apiKey: "AIzaSyBA_iU71YyDuJjAj-RbOkw1FfhTzCHCUxQ",
     authDomain: "trainscheduler-65563.firebaseapp.com",
@@ -7,9 +6,39 @@
     storageBucket: "trainscheduler-65563.appspot.com",
     messagingSenderId: "11975148"
   };
-
   firebase.initializeApp(config);
 
-  firebase.database().ref().on("value", function(snapshot){
+  var db =  firebase.database();
+  var trainName = "";
+  var destination = "";
+  var frequency = "";
+  var arrivalTime = "";
+  var minutesAway = "";
+
+
+  db.ref().on("value", function(snapshot){
     console.log(snapshot.val());
+  })  
+
+  $('#submit').on("click", function(){
+    trainName = $('#nameinput').val().trim();
+    destination = $('#firsttimeinput').val().trim();
+    frequency = $('#frequencyinput').val().trim();
+    arrivalTime = $('#arrivalinput').val().trim();
+    minutesAway = $('#minutesinput').val().trim();
+  
+    db.ref().set({
+      trainName: trainName
+    });
+
+    return false;
+  });
+
+  db.ref().on("value", function(snapshot) {
+  console.log(snapshot.val());
+  console.log(snapshot.val().trainName);
+
   })
+
+ 
+
